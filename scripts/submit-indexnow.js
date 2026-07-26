941e420e18e772d7b633e3b4a668c3f22285f096
Wender Silva Rodrigues <silvarodrigueswender30@gmail.com>
2026-07-26 12:28:57 -0300

    feat: configura IndexNow com chave verificada e utilitário de envio

---
+++ scripts/submit-indexnow.js
@@ -0,0 +1,46 @@
+// execute com: node scripts/submit-indexnow.js
+
+const INDEXNOW_KEY = '90a859fdfdbc4145bb275ea7d256e75b';
+const INDEXNOW_HOST = 'www.offdata.digital';
+
+const urls = [
+  'https://www.offdata.digital/',
+  'https://www.offdata.digital/agencia',
+  'https://www.offdata.digital/blog',
+  'https://www.offdata.digital/criacao-de-sites-uberlandia',
+  'https://www.offdata.digital/seo-local-uberlandia',
+  'https://www.offdata.digital/trafego-pago-uberlandia',
+  'https://www.offdata.digital/landing-pages-high-ticket',
+  'https://www.offdata.digital/performance-web',
+  'https://www.offdata.digital/conteudo-tecnico',
+  'https://www.offdata.digital/automacao-comercial-b2b',
+  'https://www.offdata.digital/estrategias/seo-local-imobiliarias-luxo',
+  'https://www.offdata.digital/estrategias/sites-imersivos-clinicas-medicas',
+  'https://www.offdata.digital/estrategias/trafego-alta-intencao-b2b',
+  'https://www.offdata.digital/estrategias/performance-web-marcas-high-ticket',
+  'https://www.offdata.digital/estrategias/criacao-de-site-para-imobiliaria',
+  'https://www.offdata.digital/estrategias/criacao-de-site-para-clinica',
+  'https://www.offdata.digital/estrategias/criacao-de-loja-virtual',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-advogados',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-arquitetos',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-construtora',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-encanadores',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-imobiliaria',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-loja-de-roupa',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-loja-de-tenis',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-paisagistas',
+  'https://www.offdata.digital/blog/criacao-de-sites-uberlandia-para-psicologos',
+];
+
+fetch('https://api.indexnow.org/indexnow', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json; charset=utf-8' },
+  body: JSON.stringify({
+    host: INDEXNOW_HOST,
+    key: INDEXNOW_KEY,
+    keyLocation: `https://${INDEXNOW_HOST}/${INDEXNOW_KEY}.txt`,
+    urlList: urls,
+  }),
+})
+.then(r => console.log('[IndexNow] Status:', r.status))
+.catch(e => console.error('[IndexNow] Erro:', e));
